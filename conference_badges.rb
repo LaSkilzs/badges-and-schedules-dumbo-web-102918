f0d1e5658db799d857e560ae51d2c41c9b321f91
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-12 22:45:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,8 +8,7 @@ end
 
 def assign_rooms(array)
   result = []
-  room = 0
-  array.each{|name|result << "Hello, #{name}! You'll be assigned to room 1!"}
+  array.each_with_index{|name, room_num|result << "Hello, #{name}! You'll be assigned to room #{room_num}!"}
   result
 end
 
