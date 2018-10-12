cf4d7ec6930916f5cea4a7a2d5848acccc3045c9
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-12 22:41:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,12 @@ end
 def batch_badge_creator(array)
   result = array.map{|string|badge_maker(string)}
 end
+
+def assign_rooms(array)
+end
+
+
+
+
+# expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
+#     end
